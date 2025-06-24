74755d0a6ae3c3748baa483d5c947df68dbb08cb
Aakanshi-garg <145046018+Aakanshi-garg@users.noreply.github.com>
2025-06-25 01:42:20 +0530

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -3,4 +3,4 @@ sudo dnf update -y
 sudo dnf install -y nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
-echo "<h1> Hi!! I'm Aakanshi Garg . </h1>" > /usr/share/nginx/html/index.html
+echo "<h1> Hi!! I'm Aakanshi Garg . </h1>" > /var/www/html/index.html
